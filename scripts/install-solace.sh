30b7ba2f5999f7ed53c5369ecdd9279697a1e0a9
KenBarr <ken.barr@solace.com>
2017-08-10 15:20:35 -0400

    Re-jig security groups, reduce networking options relying on defaults, remove monitor storage options, eliminate need to access vpn cidr , limit IAM role access

--- scripts/install-solace.sh
+++ scripts/install-solace.sh
@@ -57,19 +57,45 @@ export admin_password=`cat ${admin_password_file}`
 rm ${admin_password_file}
 mkdir $solace_directory
 cd $solace_directory
-echo "`date` Configure VMRs Started"
+echo "`date` INFO: Configure VMRs Started"
+
+
+echo "`date` INFO: check to make sure we have a complete load"
+wget -O /tmp/solos.info -nv  https://products.solace.com/download/VMR_DOCKER_EVAL_MD5
+IFS=' ' read -ra SOLOS_INFO <<< `cat /tmp/solos.info`
+MD5_SUM=${SOLOS_INFO[0]}
+SolOS_LOAD=${SOLOS_INFO[1]}
+echo "`date` INFO: Reference md5sum is: ${MD5_SUM}"
 
 wget -q -O solace-redirect ${solace_url}
 REAL_LINK=`egrep -o "https://[a-zA-Z0-9\.\/\_\?\=]*" ${solace_directory}/solace-redirect`
-wget -q -O soltr-docker.tar.gz ${REAL_LINK}
+wget -q -O ${SolOS_LOAD} ${REAL_LINK}
+LOCAL_MD5_SUM=`md5sum /tmp/${SolOS_LOAD}`
+if [ ${LOCAL_MD5_SUM} -ne `cat /tmp/solos.info` ]; then
+    echo "`date` WARN: Possible corrupt SolOS load, md5sum do not match"
+else
+    echo "`date` INFO: Successfully downloaded ${SolOS_LOAD}"
+fi
+
+
 
 # Make sure Docker is actually up
 docker_running=""
-while [[ ${docker_running} != "running" ]]; do 
-    echo "ERROR: Tried to launch Solace but Docker was not up"
+loop_guard=6
+loop_count=0
+while [[ ${loop_count} -ne ${loop_guard} ]]; do 
     sleep 10
     docker_running=`service docker status | grep -o running`
+    if [ ${docker_running} -ne "running" ]; then
+        ((loop_count++))
+        echo "`date` WARN: Tried to launch Solace but Docker in state ${docker_running}"
+    else
+        echo "`date` INFO: Docker in state ${docker_running}"
+        break
+    fi
 done
+
+
 
 docker load -i ${solace_directory}/soltr-docker.tar.gz
 
