2da22e0c263e8e30b7047ed256963507a57618f3
bczoma <balazs.czoma@solace.com>
2020-05-11 06:49:59 -0400

    Updated with support of PubSub+ 9.4 and later

--- scripts/gen-cluster-hosts.sh
+++ scripts/gen-cluster-hosts.sh
@@ -20,14 +20,14 @@
 # instance.
 #
 # The assumption is that the Cloudformation Stack deploys 2 autoscaling groups.
-#	MessageBroker	(/tmp/routers)
+#	EventBroker	(/tmp/routers)
 #	MonitorNodes	(/tmp/monitors)
 #
 # The complete list of hosts for this stack is saved
 # to SOLACE_HOSTS_FILE (default is /tmp/solacehosts)
 
 
-murl_top=http://instance-data/latest/meta-data
+murl_top=http://169.254.169.254/latest/meta-data
 
 SOLACE_HOSTS_FILE=${SOLACE_HOSTS_FILE:-/tmp/solacehosts}
 
@@ -85,9 +85,9 @@ done
 # The different models will have slightly different labels for the 
 # nodes associated with each group ... but it's simple to handle both cases.
 #pwd
-grep -q -e "-MessageBroker.*Stack-" ${SOLACE_HOSTS_FILE}
+grep -q -e "-EventBroker.*Stack-" ${SOLACE_HOSTS_FILE}
 if [ $? -eq 0 ] ; then
-    grep -e "-MessageBroker.*Stack-" ${SOLACE_HOSTS_FILE} \
+    grep -e "-EventBroker.*Stack-" ${SOLACE_HOSTS_FILE} \
     | awk '{print $1" ROUTERNODE"NR-1" "$2" "$3" "$4}' > /tmp/routers
 else
     cp ${SOLACE_HOSTS_FILE} /tmp/routers
